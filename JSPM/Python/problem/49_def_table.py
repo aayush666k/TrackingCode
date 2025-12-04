4e4e612b6397dc576a77e3853f5b11c607267e28
aayush666k <aayush666k@gmail.com>
2025-12-04 15:29:21 +0530

    Add Python problems and update C example
    Added two new Python scripts: one for printing a multiplication table (49_def_table.py) and another for checking the sign of a number (50_def_num_sign.py). Updated tempCodeRunnerFile.c in Chapter 4 to demonstrate increment operations and value changes for variable i.

---
+++ JSPM/Python/problem/49_def_table.py
@@ -0,0 +1,5 @@
+def table():
+    a = int(input("Enter a number :"))
+    for i in  range (1,11):
+      print(f"{a}X{i}={a*i}")
+table()
--- c_programing/Chapter 4 Loop and Instruction/tempCodeRunnerFile.c
+++ c_programing/Chapter 4 Loop and Instruction/tempCodeRunnerFile.c
@@ -1,4 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    int i = 5;
+    printf("The value after i++ is %d\n", i++);
+    printf("The value after ++i is %d\n", ++i);
+    printf("The value after i is %d\n", i);
+    i += 13;
+    printf("The value after i is %d\n", i);
 
     return 0;
 }
